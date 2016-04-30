a7c84225c7cee530c623dda94eb9b942a19a33be
GAylabea <gbea@rocketmail.com>
2016-04-30 13:19:48 -0500

    created quiz.js file to make seperate from the test.js file - added comments

--- quiz.js
+++ quiz.js
@@ -1,37 +1,20 @@
-"use strict"
+// "use strict"
 
-function showCars (inventory) {
-  // Loop over the inventory and populate the page
-   let outputString = ""; 
-    let carDom = document.getElementById("cars"); 
-    for (let i = 0; i < inventory.length; i++) {
-    let currentInventory = inventory[i]; 
-    outputString += `<h1>${currentInventory.make}<h1>`; 
-    // <h5>{currentCarList.model} </h5><h5>{currentCarList.year} </h5><h5>{currentCarList.price} </h5><h5>{currentCarList.color} </h5>
-    // <h5>{currentCarList.purchased} </h5><h5>{currentCarList.description} </h5>`; 
-  }
-    carDom.innerHTML += outputString; 
-  }; 
+function listInventory (inventory) {
+       // list in the DOM - turn the html element where the car cards will go into a variable
+      var carDom = document.getElementById("container"); 
+      // variable to leave the outputString blank until it is filled with car card info - the DOM string 
+      var outputString = "";
+      // this for loop is checking each array item in inventory to use it to build each car card
+      for (var i = 0; i <inventory.length; i++) {
+      var currentInventory = inventory[i];
+        // this is to build the DOM string - the car card - by creating html elements for each one
+      carDom.innerHTML += `<div class = "col-xs-4 card" style="border-color: ${currentInventory.color}"><h1>${currentInventory.make}</h1><h5>${currentInventory.model}</h5><h5>${currentInventory.year}</h5><h5>${currentInventory.price}</h5>${currentInventory.color}</h5><h5>${currentInventory.purchased}</h5><div class = "description"><h5>${currentInventory.description}</h5></div></div>`
+      }
+      // this calls the event listener for the borderClick event which will activate the other events in the events.js file
+      CarLot.borderClick();
+ }
 
-
-this is what works!!!!
-// function listInventory (inventory) {
-//        // list in the DOM - get the element where the car cards go
-//       var carDom = document.getElementById("container"); 
-//       var outputString = "";
-//       for (var i = 0; i <inventory.length; i++) {
-//         var currentInventory = inventory[i];
-//         // this is to build the DOM string
-//       outputString = `<h1>${currentInventory.make}</h1><h5>${currentInventory.model}</h5><h5>${currentInventory.year}</h5><h5>${currentInventory.price}</h5><h5>${currentInventory.color}</h5><h5>${currentInventory.purchased}</h5><h5>${currentInventory.description}</h5>`
-//       carDom.innerHTML += outputString;
-//       }
-// }
-  // Now that the DOM is loaded, establish all the event listeners needed
-//   CarLot.activateEvents();
-
-// }
-
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
-// carLot.loadcarLot(showcarList); 
-carLot.loadInventory(showCars); 
+// // Load the inventory and send a callback function to be
+// // invoked after the process is complete
+CarLot.loadInventory(listInventory); 
