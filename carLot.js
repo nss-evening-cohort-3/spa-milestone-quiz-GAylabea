6acffb59b5148cdbfdea34f261d295edc8aaf130
GAylabea <gbea@rocketmail.com>
2016-04-30 13:11:57 -0500

    changed carLot into original iife and made comments

--- carLot.js
+++ carLot.js
@@ -1,21 +1,25 @@
 // This is the XHR to load the JSON file into a js object
-"use strict"
+// "use strict"
 
-let carLot = (function () {
+ "use strict"
+// This is the first iife that will be augmented by the others
+var CarLot = (function () {
   // this is the JSON file
-  let inventory = [];
-  // return {
-  //   getinventory: function () {
-  //    return inventory;
-  //   },
+  var inventory = [];
+  return {
+    // this function loads the inventory array called Cars from the json file into the js file or according to the READme: 
+    // "When your page first loads, you need to use an XHR to load the contents of the JSON file, and parse them into a native 
+    // JavaScript object."
      loadInventory: function () {
-      let loader = new XMLHttpRequest();
-      // loader.open("GET", "inventory.json");
-      // loader.send(); 
+      var loader = new XMLHttpRequest();
+      loader.open("GET", "inventory.json");
+      loader.send();
       loader.addEventListener("load", function () {
-      inventory = JSON.parse(this.responseText); 
-     console.log("inventory", inventory); 
-      });
+        //this uses the name of the array, cars - not the name of the json file
+      inventory = JSON.parse(this.responseText).cars; 
+      // this calls the function listInventory and passes the inventory, the orginal array, into the function 
+      listInventory (inventory);
+    });
     }
   }
-})();
+})(); 
